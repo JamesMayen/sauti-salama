9d6d62a037f79989d40603e32313fd3bbb77f860
James Mayen <mayenjames15@gmail.com>
2026-09-18 14:46:58 +0200

    Fix Render frontend build errors

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import Hero from "../components/home/Hero";
 import ProblemSection from "../components/home/ProblemSection";
 import HowItWorks from "../components/home/HowItWorks";
 import Features from "../components/home/Features";
-import LowBandwidth from "../components/home/LowBandwidth";
+import LowBandwidth from "../components/home/LowbandWidth.jsx";
 import CallToAction from "../components/home/CallToAction";
 
 function Home() {
